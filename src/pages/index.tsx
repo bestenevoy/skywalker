d34a6553b1a76c109ec6baa93d968f70e9a12e62
wrz <fw51640@gmail.com>
2021-05-27 21:51:14 +0800

    chore(page): applying footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link'
 import Img from 'next/image'
-import {StringTyping, ICP} from 'src/components'
+import {StringTyping, Footer} from 'src/components'
 import {getRandomImage} from 'src/helpers'
+// import {useState} from 'react'
 
 export default function Home () {
   const src = getRandomImage()
@@ -9,27 +9,11 @@ export default function Home () {
     <div className="home text-center w-screen relative">
       <main className="flex flex-1 flex-col justify-center items-center">
         <div className="sm:whitespace-pre text-2xl">
-          <Img className="object-cover top-0 right-0 absolute" src={src} layout="fill" alt={src} />
+          <Img className="object-cover top-0 right-0 absolute" key={src} src={src} alt="index page background image" layout="fill" />
           <StringTyping id="string-typing" className="font-bold text-shadow text-4xl text-white mix-blend-difference" />
         </div>
       </main>
-      <footer className="z-10 flex w-full sm:block md:flex justify-center text-sm p-3">
-        <div>Copyright &copy;2020-2021 &nbsp;
-          <strong>
-            <a href="https://blog.wrz8.com">Blog</a> |
-            <span className="text-red-500">
-              &nbsp;<Link href="/about">About</Link>
-            </span>
-            .
-          </strong>
-        </div>
-        <div className="hidden sm:block">
-          <span>
-            &nbsp;Built with &nbsp;<a className="text-red-500" href="http://nextjs.org">Next.js🔥</a>.&nbsp;
-          </span>
-          <ICP />
-        </div>
-      </footer>
+      <Footer />
     </div>
   )
 }
