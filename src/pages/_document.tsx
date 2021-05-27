1b58d04b2ee368baa4294326b4dcd8559f897a3d
wrz <fw51640@gmail.com>
2021-05-28 00:58:56 +0800

    chore(pkg): update react react-dom next

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,16 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {Html, Head, Main, NextScript} from 'next/document'
+
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head></Head>
-        <body className="font-sans text-base relative bottom-0 top-0 right-0 left-0">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+    render () {
+        return (
+            <Html>
+                <Head />
+                <body className="font-sans text-base relative bottom-0 top-0 right-0 left-0">
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
