9234f8fc563bf11715ef2405c3fff9b40d5db116
wrz <fw51640@gmail.com>
2021-05-28 12:06:59 +0800

    chore(page): use Image and Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,28 @@
 import type {AppProps /*, AppContext */} from 'next/app'
 import {useEffect} from 'react'
 import Head from 'next/head'
-import {FrontendStatic, getAnalyticsTag} from 'src/helpers'
+import {FrontendStatic} from 'src/helpers'
+import {Analytics} from 'src/components'
 
 import 'src/styles/globals.scss'
 
 function MyApp ({Component, pageProps}: AppProps) {
-  useEffect(() => {
-    window.EC = FrontendStatic
-  })
-  return (
-    <>
-      <Head>
-        <title>Bestenevoy</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="robots" content="all" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-        <script dangerouslySetInnerHTML={getAnalyticsTag()} />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+    useEffect(() => {
+        window.__APP__ = FrontendStatic
+    })
+    return (
+        <>
+            <Head>
+                <title>Bestenevoy</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="robots" content="all" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="manifest" href="/manifest.json" />
+                {Analytics()}
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,18 @@
-import Img from 'next/image'
-import {StringTyping, Head, Footer} from 'src/components'
+import {StringTyping, Head, Footer, Image} from 'src/components'
 import {getRandomImage} from 'src/helpers'
 
 export default function Home () {
-  const src = getRandomImage()
-  return (
-    <div className="home min-h-screen flex flex-col select-none text-center w-screen relative">
-      <Head title="Home" />
-      <main className="flex flex-1 flex-col justify-center items-center">
-        <div className="sm:whitespace-pre text-2xl">
-          <Img className="object-cover top-0 right-0 absolute" key={src} src={src} alt="index page background image" layout="fill" />
-          <StringTyping id="string-typing" className="font-bold text-shadow text-4xl text-white mix-blend-difference" />
+    const src = getRandomImage()
+    return (
+        <div className="home bg-black min-h-screen flex flex-col select-none text-center w-screen relative">
+            <Head title="Home" />
+            <main className="flex flex-1 flex-col justify-center items-center">
+                <div className="sm:whitespace-pre text-2xl">
+                    <Image className="object-cover top-0 right-0 absolute" key={src} src={src} alt="index page background image" layout="fill" />
+                    <StringTyping id="string-typing" className="font-bold text-shadow text-4xl text-white mix-blend-difference" />
+                </div>
+            </main>
+            <Footer />
         </div>
-      </main>
-      <Footer />
-    </div>
-  )
+    )
 }
