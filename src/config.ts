d71a5aec8534c34d460e05543ca2c37212043122
wrz <fw51640@gmail.com>
2021-05-29 02:32:25 +0800

    chore(pkg): modify images config

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,7 @@
 
 export const HOME_TEXT = ["Life is like a box of chocolates, \n you never know what you are going to get. \n ——Forrest Gump"]
 export const IMAGES = [
-    '/home.jpg',
-    'http://qiniu.wrz8.com/image/z8p9rj.jpeg',
-    'http://qiniu.wrz8.com/image/e7821r.jpeg',
-    'http://qiniu.wrz8.com/image/x86eko.jpeg'
+    'http://qiniu.wrz8.com/image/ymwmek.jpeg',
+    'http://qiniu.wrz8.com/image/r2jedw-1.jpeg',
+    'http://qiniu.wrz8.com/image/I3zqvp-1.webp',
 ]
