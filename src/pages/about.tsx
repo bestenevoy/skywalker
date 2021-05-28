86326e2379e610be923aba6a7e4231f102bb6d01
wrz <fw51640@gmail.com>
2021-05-28 14:01:43 +0800

    chore(page): about add head

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,9 @@
-import {Header} from 'src/components'
+import {Head, Header} from 'src/components'
 
 export default function Home () {
     return (
         <div className="h-screen flex flex-col">
+            <Head title="About me" />
             <Header className="w-1/2"></Header>
             <div className="flex justify-center items-center flex-col flex-grow">
                 <main className="w-1/2 m-auto leading-6">
