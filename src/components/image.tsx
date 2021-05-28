9111bae073f60cbb494f3e9d83bae6727ea81f1b
wrz <fw51640@gmail.com>
2021-05-28 10:51:44 +0800

    feat(component): add image extend next/image

---
+++ src/components/image.tsx
@@ -0,0 +1,18 @@
+import {ReactEventHandler} from "react"
+import type {ImageProps} from 'next/image'
+import Image from 'next/image'
+
+type IProps = ImageProps & {
+    download?: boolean
+}
+
+export default function CustomImage (props: IProps) {
+    const {download} = props
+    const display: ReactEventHandler<HTMLImageElement> = (e) => {
+        e.preventDefault && e.preventDefault()
+    }
+    const prop = Object()
+    download && Object.assign(prop, {onDragStart: display, onContextMenu: display})
+    const newProps = Object.assign(prop, props)
+    return <>{Image(newProps)}</>
+}
